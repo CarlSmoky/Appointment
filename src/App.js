75d18250402f3608cd5553493f521a3004db6785
tsumitakaoru <k.tsumita01@gmail.com>
2022-02-28 17:32:59 -0500

    Add files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { BiArchive } from "react-icons/bi"
+
+
+function App() {
+  return (
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl"><BiArchive className="inline-block text-red-300 align-top" /> My Appointment</h1>
+    </div>
+  );
+}
+
+export default App;
