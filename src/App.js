87493298b9eed75e0f5cfbc88898b4b2c667881d
tsumitakaoru <k.tsumita01@gmail.com>
2022-03-02 20:20:49 -0500

    Add AddAppointments components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BiArchive } from "react-icons/bi"
+import AddAppointments from "./components/AddAppointments";
 import { Search } from "./components/Search";
 
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl">
         <BiArchive className="inline-block text-red-300 align-top" /> My Appointment</h1>
+        <AddAppointments />
         <Search />
     </div>
   );
