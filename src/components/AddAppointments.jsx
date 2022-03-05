90cc23ff9736792e5570116d304879ea938f75f8
tsumitakaoru <k.tsumita01@gmail.com>
2022-03-05 16:06:56 -0500

    Change Style

--- src/components/AddAppointments.jsx
+++ src/components/AddAppointments.jsx
@@ -9,6 +9,7 @@ const AddAppointments = ({ onSendAppointment, lastId }) => {
     aptTime: "",
     aptNotes: ""
   }
+
   let [toggleForm, setToggleForm] = useState(false);
   let [formData, setFormData] = useState(clearData);
 
@@ -29,10 +30,10 @@ const AddAppointments = ({ onSendAppointment, lastId }) => {
     <div>
       <button
         onClick={() => { setToggleForm(!toggleForm) }}
-        className={`bg-blue-400 text-white px-2 py-3 w-full text-left rounded-t-md ${toggleForm ? 'rounded-t-md' : 'rounded-md'}`}>
+        className={`bg-teal-800/60 text-white px-2 py-3 w-full text-left hover:bg-teal-800/70 rounded-t-md ${toggleForm ? 'rounded-t-md' : 'rounded-md'}`}>
         <div><BiCalendarPlus className="inline-block align-text-top" />  Add Appointment</div>
       </button>
-      {toggleForm && <div className="border-r-2 border-b-2 border-l-2 border-light-blue-500 rounded-b-md pl-4 pr-4 pb-4">
+      {toggleForm && <div className="border-r-2 border-b-2 border-l-2 border-stone-500 rounded-b-md pl-4 pr-4 pb-4">
         <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
           <label htmlFor="ownerName" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
             Owner Name
@@ -44,7 +45,8 @@ const AddAppointments = ({ onSendAppointment, lastId }) => {
               id="ownerName"
               onChange={(event) => { setFormData({ ...formData, ownerName: event.target.value }) }}
               value={formData.ownerName}
-              className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
+              className="max-w-lg block w-full shadow-sm focus:ring-stone-500 focus:border-yellow
+              -500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
           </div>
         </div>
 
@@ -59,7 +61,7 @@ const AddAppointments = ({ onSendAppointment, lastId }) => {
               id="petName"
               onChange={(event) => { setFormData({ ...formData, petName: event.target.value }) }}
               value={formData.petName}
-              className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
+              className="max-w-lg block w-full shadow-sm focus:ring-stone-500 focus:border-stone-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
           </div>
         </div>
 
@@ -89,7 +91,7 @@ const AddAppointments = ({ onSendAppointment, lastId }) => {
               id="aptTime"
               onChange={(event) => { setFormData({ ...formData, aptTime: event.target.value }) }}
               value={formData.aptTime}
-              className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
+              className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-yellow-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
           </div>
         </div>
 
@@ -114,7 +116,7 @@ const AddAppointments = ({ onSendAppointment, lastId }) => {
             <button
               type="submit"
               onClick={formDataPublish}
-              className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-400 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-400"
+              className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-teal-800/60 hover:bg-teal-800/70 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-stone-400"
             >
               Submit
             </button>
--- src/components/AppointmentInfo.jsx
+++ src/components/AppointmentInfo.jsx
@@ -3,14 +3,14 @@ export const AppoinmentInfo = ({ appointment, onDeleteAppointment }) => {
   return (
     <li className="px-3 py-3 flex items-start">
       <button onClick={() => onDeleteAppointment(appointment.id)} type="button"
-        className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-400 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+        className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-400/60 hover:bg-red-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
         <BiTrash /></button>
       <div className="flex-grow">
         <div className="flex items-center">
-          <span className="flex-none font-medium text-2xl text-blue-500">{appointment.petName}</span>
+          <span className="flex-none font-medium text-2xl text-zinc-700/70">{appointment.petName}</span>
           <span className="flex-grow text-right">{appointment.aptDate}</span>
         </div>
-        <div><b className="font-bold text-blue-500">Owner:</b> {appointment.ownerName}</div>
+        <div><b className="font-bold text-zinc-700/70">Owner:</b> {appointment.ownerName}</div>
         <div className="leading-tight">{appointment.aptNotes}</div>
       </div>
     </li>
